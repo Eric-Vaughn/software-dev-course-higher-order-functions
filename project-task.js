37e2e7e931e47e99b26c5909161150a0bceec042
oraclemoss <vaughneric4@gmail.com>
2026-09-14 14:56:54 -0500

    chore remove unnecessary boolean check

--- project-task.js
+++ project-task.js
@@ -110,7 +110,7 @@ Step-by-Step:
 */
 
 const totalStockValue = products.reduce((sum, product) => {
-  return product.inStock === true ? sum + product.price : sum + 0;
+  return product.inStock ? sum + product.price : sum + 0;
 }, 0);
 
 console.log("Total value in stock:", totalStockValue);
